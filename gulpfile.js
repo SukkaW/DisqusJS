f2961fa11de9c6986763f34f384b5c4cbeda3451
SukkaW <isukkaw@gmail.com>
2018-10-07 12:32:49 +0800

    chore(gulp): add minify-css function

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
 let gulp = require('gulp');
 let uglify = require('gulp-uglify');
+let autoprefixer = require('gulp-autoprefixer');
+let cleanCSS = require('gulp-clean-css');
 
-gulp.task('build', () => {
+var configs = {
+    autoprefixer: {
+        browsers: [
+            'last 2 versions',
+            '> 1%',
+            'Chrome >= 30',
+            'Firefox >= 30',
+            'ie >= 9',
+            'Safari >= 8',
+        ],
+    },
+    cleanCSS: {
+        compatibility: 'ie10'
+    },
+};
+
+gulp.task('minify-js', () => {
     return gulp.src('src/**/*.js')
         .pipe(uglify({
             output: {
@@ -10,5 +28,14 @@ gulp.task('build', () => {
         }))
         .pipe(gulp.dest('dist'));
 });
+
+gulp.task('minify-css', () => {
+    return gulp.src('src/**/*.css')
+        .pipe(autoprefixer(configs.autoprefixer))
+        .pipe(cleanCSS(configs.cleanCSS))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', gulp.parallel('minify-js', 'minify-css'));
 
 gulp.task('default', gulp.parallel('build'));
