7aa114b4fb88e640fcc5dd1130e0f6ecacea8aab
SukkaW <isukkaw@gmail.com>
2018-10-12 15:43:03 +0800

    fix: date format

--- src/disqus.js
+++ src/disqus.js
@@ -75,7 +75,18 @@
      * Description: Transoform Date object from XML format to human readable format
      */
     function dateFormat(date) {
-        return `${date.getUTCFullYear().toString()}/${(date.getUTCMonth() + 1).toString()}/${date.getUTCDate()} ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`;
+        let y = date.getFullYear();
+        let m = date.getMonth() + 1;
+        m = m < 10 ? ('0' + m) : m;
+        let d = date.getDate();
+        d = d < 10 ? ('0' + d) : d;
+        let h = date.getHours() + 8;
+        h = h < 10 ? ('0' + h) : h;
+        let minute = date.getMinutes();
+        minute = minute < 10 ? ('0' + minute) : minute;
+        let second = date.getSeconds();
+        second = minute < 10 ? ('0' + second) : second;
+        return `${y}-${m}-${d} ${h}:${minute}:${second}`;
         //yyyy-MM-dd hh:mm:ss
     }
 
