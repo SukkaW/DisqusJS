b8d4b24e689b6284150de09fa84ef7b620088d05
SukkaW <isukkaw@gmail.com>
2018-10-20 15:49:43 +0800

    fix(regex): simplify the hrefMatcher

--- src/disqus.js
+++ src/disqus.js
@@ -417,7 +417,7 @@ function DisqusJS(config) {
             let removeDisqUs = (msg) => {
                 // aMatcher - 只处理 Disqus 短链接
                 var aMatcher = new RegExp(/<a.+?href=\"https:\/\/disq.us(.+?)\".*>/gi),
-                    hrefMatcher = new RegExp(/href=\"(?<href>[^\"]*)\"/gi)
+                    hrefMatcher = new RegExp(/href=\"(.+?)\"/gi)
                 let link = (msg.match(aMatcher) || [])
                 link.map((link) => {
                     // (.*) 是贪婪处理，会一致匹配到最后，可以用于匹配最后一次
