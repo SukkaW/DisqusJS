160bfad311b5a155fe78004c7dc5156244279fa4
SukkaW <isukkaw@gmail.com>
2022-04-18 10:08:19 +0800

    refactor: replace swr with self-written fetch hook

--- src/hooks/useDisqusApi.ts
+++ src/hooks/useDisqusApi.ts
@@ -1,11 +1,10 @@
 import { useAtomValue } from 'jotai';
-import useSWRImmutable from 'swr/immutable';
-import useSWRInfinite, { type SWRInfiniteKeyLoader } from 'swr/infinite';
 import { disqusJsApiFetcher } from '../lib/util';
 import { disqusjsSortTypeAtom } from '../state';
 import type { DisqusAPI } from '../types';
+import { useFetch, useFetchInfinite } from './useFetch';
 
-export const useDisqusThread = (shortname: string, identifier: string, apiKeys: string[], api = 'https://disqus.com/api/') => {
+export const useDisqusThread = (shortname: string, identifier: string | undefined, apiKeys: string[], api = 'https://disqus.com/api/') => {
   /*
    * 获取 Thread 信息
    * Disqus API 只支持通过 Thread ID 获取评论列表，所以必须先通过 identifier 获取当前页面 Thread ID
@@ -13,17 +12,17 @@ export const useDisqusThread = (shortname: string, identifier: string, apiKeys: 
    * API Docs: https://disqus.com/api/docs/threads/list/
    * API URI: /3.0/threads/list.json?forum=[disqus_shortname]&thread=ident:[identifier]&api_key=[apikey]
    */
-  return useSWRImmutable<DisqusAPI.Thread>(`${api}3.0/threads/list.json?forum=${encodeURIComponent(shortname)}&thread=${encodeURIComponent(`ident:${identifier}`)}`, disqusJsApiFetcher(apiKeys));
+  return useFetch<DisqusAPI.Thread>(typeof identifier === 'string' ? `${api}3.0/threads/list.json?forum=${encodeURIComponent(shortname)}&thread=${encodeURIComponent(`ident:${identifier}`)}` : null, disqusJsApiFetcher(apiKeys));
 };
 
 export const useDisqusPosts = (shortname: string, id: string | null, apiKeys: string[], api = 'https://disqus.com/api/') => {
   const sortType = useAtomValue(disqusjsSortTypeAtom);
 
-  const getKey: SWRInfiniteKeyLoader = (pageIndex: number, previousData) => {
+  const getKey = (pageIndex: number, previousData: DisqusAPI.Posts | undefined) => {
     if (!id) return null;
     if (previousData && !previousData.cursor.hasNext) return null;
     if (pageIndex === 0) return `${api}3.0/threads/listPostsThreaded?forum=${shortname}&thread=${id}&order=${sortType ?? 'desc'}`;
-    return `${api}3.0/threads/listPostsThreaded?forum=${shortname}&thread=${id}${previousData.cursor.next ? `&cursor=${previousData.cursor.next}` : ''}&order=${sortType ?? 'desc'}`;
+    return `${api}3.0/threads/listPostsThreaded?forum=${shortname}&thread=${id}${previousData?.cursor.next ? `&cursor=${previousData.cursor.next}` : ''}&order=${sortType ?? 'desc'}`;
   };
   /*
    * 获取评论列表
@@ -47,13 +46,8 @@ export const useDisqusPosts = (shortname: string, id: string | null, apiKeys: st
    * 每次加载翻页评论的时候合并数据并进行重排序
    * 用户切换排序方式的时候直接取出进行重新渲染
    */
-  return useSWRInfinite<DisqusAPI.Posts>(
+  return useFetchInfinite<DisqusAPI.Posts>(
     getKey,
-    disqusJsApiFetcher(apiKeys),
-    {
-      revalidateIfStale: false,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false
-    }
+    disqusJsApiFetcher(apiKeys)
   );
 };
