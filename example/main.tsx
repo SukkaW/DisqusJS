08dc2776ab7d6b9f54523ad1e9ae0d92a62088ad
SukkaW <isukkaw@gmail.com>
2025-03-24 22:27:33 +0800

    chore(dev): test against react strict mode

--- example/main.tsx
+++ example/main.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { StrictMode } from 'react';
 
 const container = document.getElementById('app');
 if (container) {
   const root = createRoot(container);
-  root.render(<App />);
+  root.render(<StrictMode><App /></StrictMode>);
 }
